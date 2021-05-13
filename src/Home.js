139abddfcd31363a098d1af7d2ece3b2e3031ef4
Ajiharan <baskaranajiharan1243@gmail.com>
2021-05-14 01:30:42 +0530

    changes added

--- src/Home.js
+++ src/Home.js
@@ -3,36 +3,74 @@ import { io } from "socket.io-client";
 import ChatContainer from "./container/ChatContainer";
 import Navbar from "./navbar/Navbar";
 import "./App.css";
+import TextContainer from "./TextContainer/TextContainer";
 let socket = "";
 const Home = (props) => {
   const ENDPOINT = "http://localhost:5000";
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [users, setUsers] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
   useEffect(() => {
     if (props?.location?.state) {
-      console.log("search", props.location.state);
+      console.log("search", props.location);
       setRoom(props.location.state.room);
-      setName(props.location.name);
-      socket = io(ENDPOINT, { withCredentials: true });
-
-      socket.emit("join", { name, room }, () => {});
-      return () => {
-        socket.emit("disconnect");
-        socket.off();
-      };
+      setName(props.location.state.name);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
-    props.location.name,
     props.location.state,
     props.location.state.name,
     props.location.state.room,
   ]);
 
+  useEffect(() => {
+    socket = io(ENDPOINT, { withCredentials: true });
+    if (name !== "" && room !== "") {
+      socket.emit("join", { name, room }, () => {});
+    }
+
+    return () => {
+      if (socket) {
+        socket.emit("removeUser");
+        socket.off();
+      }
+    };
+  }, [name, room]);
+  useEffect(() => {
+    socket.on("roomData", ({ users }) => {
+      console.log("users", users);
+      setUsers(users);
+    });
+
+    socket.on("message", (message) => {
+      console.log("user", message);
+      setMessages([...messages, message]);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [name, room]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    socket.emit("sendMessage", message, () => {
+      setMessages("");
+    });
+    //console.log(message);
+  };
+
   return (
-    <div className="container m-4">
-      <Navbar room={room} />
-      <ChatContainer />
+    <div className="App__container">
+      <div className="container m-4">
+        <Navbar room={room} />
+        <ChatContainer
+          messageHandler={sendMessage}
+          messages={messages}
+          setMessage={setMessage}
+          message={message}
+        />
+      </div>
+      <TextContainer users={users} />
     </div>
   );
 };
--- src/container/ChatContainer.js
+++ src/container/ChatContainer.js
@@ -1,7 +1,24 @@
 import React from "react";
 import "./ChatContainer.css";
-const ChatContainer = () => {
-  return <div className="chat-container"></div>;
+const ChatContainer = ({ messages, messageHandler, setMessage, message }) => {
+  return (
+    <div className="chat">
+      <div className="chat-container"></div>
+      <form className="text__input" onSubmit={(e) => messageHandler(e)}>
+        <input
+          type="text"
+          className="input-control"
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          value={message}
+        />
+        <button type="submit" className="btn btn-primary">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default ChatContainer;
